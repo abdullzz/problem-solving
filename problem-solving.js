047c80e0f6503a0741b4c605229a89858f4d1348
Amil Hasbala <amilhasbala@gmail.com>
2019-10-28 18:47:23 +0700

    revisi varaiable temp

--- problem-solving.js
+++ problem-solving.js
@@ -9,13 +9,13 @@ LOOP for i eq tanggalMulai until 31
     ELSE 
         STORE yangDateng as empty array
         IF (i - tanggalMulai) modulo 2 eq to 0
-            PUSH string Tono to yangDateng array
+            INSERT string Tono to yangDateng array
         END IF
         IF (i - tanggalMulai) modulo 4 eq to 0
-            PUSH string Anton to yangDateng array
+            INSERT string Anton to yangDateng array
         END IF
         IF (i - tanggalMulai) modulo 5 eq to 0
-            PUSH string Budi to yangDateng array
+            INSERT string Budi to yangDateng array
         END IF
         ADD array yangDateng joined by ", " to temp
     END IF
@@ -25,9 +25,9 @@ END LOOP
 
 var tanggalMulai = 7;
 for (let i = tanggalMulai; i <= 31; i++) {
-  var temp = "Tanggal " + i + ": ";
+  var today = "Tanggal " + i + ": ";
   if (i % 5 === 0) {
-    temp += "Tempat fitness Tutup";
+    today += "Tempat fitness Tutup";
   } else {
     var yangDateng = [];
     if ((i - tanggalMulai) % 2 === 0) {
@@ -39,7 +39,7 @@ for (let i = tanggalMulai; i <= 31; i++) {
     if ((i - tanggalMulai) % 5 === 0) {
       yangDateng.push("Budi");
     }
-    temp += `${yangDateng.join(", ")}`;
+    today += `${yangDateng.join(", ")}`;
   }
-  console.log(temp);
+  console.log(today);
 }
